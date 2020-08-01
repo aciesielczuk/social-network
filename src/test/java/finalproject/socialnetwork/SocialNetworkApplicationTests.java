e0c4c861418336e6b7f58165c6d4e7bcc418d9bf
aciesielczuk <56974114+aciesielczuk@users.noreply.github.com>
2020-08-01 13:11:01 +0200

    Init commit

---
+++ src/test/java/finalproject/socialnetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
