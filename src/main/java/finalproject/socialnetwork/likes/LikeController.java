6134f0fa540869d8269ff6ac7f5f358ed800b020
aciesielczuk <56974114+aciesielczuk@users.noreply.github.com>
2020-09-04 17:26:13 +0200

    fix: CrossOrigin annotation fixed

--- src/main/java/finalproject/socialnetwork/likes/LikeController.java
+++ src/main/java/finalproject/socialnetwork/likes/LikeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import java.util.Optional;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://snetwork-app.herokuapp.com"})
 public class LikeController {
 
     LikeService likeService;
--- src/main/java/finalproject/socialnetwork/users/UserController.java
+++ src/main/java/finalproject/socialnetwork/users/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://snetwork-app.herokuapp.com"})
 public class UserController {
 
     private UserService userService;
